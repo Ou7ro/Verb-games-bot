765eb2772799728af51246f72b3dc0e1207d444b
Ou7ro <qew.qweqwe.2016@mail.ru>
2025-09-14 01:07:14 +0500

    initial commit

---
+++ logger.py
@@ -0,0 +1,14 @@
+import logging
+
+
+def setup_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    console_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(console_formatter)
+    logger.addHandler(console_handler)
+
+    return logger
---
+++ main.py
@@ -0,0 +1,42 @@
+from logger import setup_logger
+from environs import env
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    update.message.reply_markdown_v2(
+        'Здравствуйте',
+    )
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    """Start the bot."""
+    env.read_env()
+    tg_bot_token = env.str('TG_BOT_TOKEN')
+
+    logger = setup_logger()
+    logger.info('Запуск скрипта')
+
+    updater = Updater(tg_bot_token)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
